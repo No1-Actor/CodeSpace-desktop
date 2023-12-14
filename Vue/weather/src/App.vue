e2d71219fecb427d8c9edc5eb3e6b04111145208
洪凯 <595286168@qq.com>
2023-12-14 22:56:38 +0800

    'weather'

---
+++ Vue/weather/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- 所有配了路由.vue的文件都展示在这个页面上 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css">
+*{
+    margin: 0;
+    padding: 0;
+}
+</style>
